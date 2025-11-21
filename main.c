11a88824573ca74c4663ec5b6a165545900de885
Elhaam <e4elhaam@gmail.com>
2025-11-22 02:37:34 +0500

    feat: listen done

--- main.c
+++ main.c
@@ -3,8 +3,12 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 
+// what is the diff between this and the port var?
+#define LISTEN_BACKLOG 50
+
 // I want to impleent streams(SSE), headers and async as well
 int main() {
+  const int PORT = 8080;
   int fd = socket(AF_INET, SOCK_STREAM, 0);
   if (fd < 0) {
     // perror basically logs out the values in the errno, errno holds the error
@@ -18,15 +22,27 @@ int main() {
   struct sockaddr_in server_address;
 
   server_address.sin_family = AF_INET;
-  server_address.sin_port = htons(8080);
+  server_address.sin_port = htons(PORT);
   server_address.sin_addr.s_addr = INADDR_ANY;
 
   int bind_result =
       bind(fd, (struct sockaddr *)&server_address, sizeof(server_address));
+
   if (bind_result < 0) {
     perror("Bind failed");
     exit(-1);
   }
 
-  printf("Bind success");
+  printf("Bind success\n");
+
+  int listen_result = listen(fd, LISTEN_BACKLOG);
+
+  // is this how error handling is done in big c apps? there must be a better
+  // way surely
+  if (listen_result < 0) {
+    perror("listen failed");
+    exit(-1);
+  }
+
+  printf("Listen success");
 }
