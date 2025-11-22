c5acb1c9f4b7690ddd53dab9fcc1470674a43a89
Elhaam <e4elhaam@gmail.com>
2025-11-23 02:31:12 +0500

    refactor: created functions for easier navigation

--- main.c
+++ main.c
@@ -4,54 +4,46 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 
-// I want to impleent streams(SSE), headers and async as well
-int main() {
-  const int PORT = 8080;
-  const int LISTEN_BACKLOG = 50;
-  int fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (fd < 0) {
+void handle_err(int result, const char *msg) {
+  if (result == EXIT_FAILURE) {
     // perror basically logs out the values in the errno, errno holds the error
     // value after a syscall
-    perror("socket connection failed");
+    perror(msg);
     exit(EXIT_FAILURE);
   }
+}
 
-  printf("Socket connection success\n");
-
+void bind_server(int fd, const int port) {
   struct sockaddr_in server_address;
 
   server_address.sin_family = AF_INET;
-  server_address.sin_port = htons(PORT);
+  server_address.sin_port = htons(port);
   server_address.sin_addr.s_addr = INADDR_ANY;
 
   int bind_result =
       bind(fd, (struct sockaddr *)&server_address, sizeof(server_address));
 
-  if (bind_result < 0) {
-    perror("Bind failed");
-    exit(EXIT_FAILURE);
-  }
+  handle_err(bind_result, "Bind failed");
 
   printf("Bind success\n");
+}
 
-  int listen_result = listen(fd, LISTEN_BACKLOG);
+void listen_for_reqs(int fd, const int port, const int listen_backlog) {
+  int listen_result = listen(fd, listen_backlog);
 
-  if (listen_result < 0) {
-    perror("listen failed");
-    exit(EXIT_FAILURE);
-  }
+  handle_err(listen_result, "listen failed");
 
-  printf("Waiting for conn at http://localhost:%i \n", PORT);
+  printf("Waiting for conn at http://localhost:%i \n", port);
+}
 
+int accept_reqs(int fd) {
   struct sockaddr_in client_address;
   socklen_t client_address_len = sizeof(client_address);
 
   int client_fd =
       accept(fd, (struct sockaddr *)&client_address, &client_address_len);
-  if (client_fd < 0) {
-    perror("Accept failed");
-    exit(EXIT_FAILURE);
-  }
+
+  handle_err(client_fd, "Accept failed");
 
   /* (inet_ntop = "internet network to presentation", ntohs = "network to host
    * short") */
@@ -60,14 +52,40 @@ int main() {
   printf("Accepted connection from %s:%d\n", client_ip,
          ntohs(client_address.sin_port));
 
-  char buffer[1024] = {0};
+  return client_fd;
+}
+typedef struct {
+  char data[1024];
+  int size;
+} ParseResult;
 
-  int req_result = recv(client_fd, buffer, sizeof(buffer), 0);
+ParseResult parse_req(int client_fd) {
+  ParseResult result = {0};
 
-  if (req_result < 0) {
-    perror("Request parsing failed\n");
-    exit(EXIT_FAILURE);
-  }
+  result.size = recv(client_fd, result.data, sizeof(result.data), 0);
 
-  printf("Request from the frontend: %s", buffer);
+  handle_err(result.size, "Request parsing failed\n");
+
+  return result;
+}
+
+// I want to impleent streams(SSE), headers and async as well
+// I want to also learn why its easy to mess up security in c as well
+int main() {
+  const int PORT = 8080;
+  const int LISTEN_BACKLOG = 50;
+
+  int fd = socket(AF_INET, SOCK_STREAM, 0);
+  handle_err(fd, "socket connection failed");
+
+  printf("Socket connection success\n");
+
+  bind_server(fd, PORT);
+  listen_for_reqs(fd, PORT, LISTEN_BACKLOG);
+
+  int client_fd = accept_reqs(fd);
+
+  ParseResult req = parse_req(client_fd);
+
+  printf("Request from the frontend: %s", req.data);
 }
